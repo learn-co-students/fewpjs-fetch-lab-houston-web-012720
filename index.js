fd3899ce483250d832c705de0e74d4e51b006680
placidowang <placidow@gmail.com>
2020-03-11 04:25:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json)); // why not return here and call renderBooks(fetchBooks()) in dom load event listener?
 }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
-  json.forEach(book => {
+  json.forEach(book => { // cannot use .forEach on json?
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
@@ -12,5 +14,8 @@ function renderBooks(json) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
+  // let books = fetchBooks();
+  // debugger
+  // renderBooks(books);
+  fetchBooks();
 })
